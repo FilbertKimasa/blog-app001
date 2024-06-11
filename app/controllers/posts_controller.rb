88a7562d89922f86305c51fb69d616e654475626
FilbertKimasa <filbertdan67@gmail.com>
2024-06-11 12:06:10 +0300

    Delete post_summary partial

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts = @user.posts.order(created_at: :desc).includes(:comments, :likes)
+    @posts = @user.posts.order(created_at: :desc).includes(:comments, :likes).limit(2)
   end
 
   def show; end
