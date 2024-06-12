e8430d0d6e7806e7657d5bd3fa109c4a34f305aa
FilbertKimasa <filbertdan67@gmail.com>
2024-06-12 15:11:03 +0300

    Add pagination

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,9 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts = @user.posts.order(created_at: :desc).includes(:comments, :likes).limit(2)
+   
+    @posts = @user.posts.order(created_at: :desc).includes(:comments, :likes).page(params[:page]).per(2)
+
   end
 
   def show; end
