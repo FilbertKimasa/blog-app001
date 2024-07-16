b0c1075482ffb027330d7f2e417ed2fbe0ae7cc2
codeCraft <filbertdan67@gmail.com>
2024-07-16 16:59:12 +0300

    Handle record not found for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_user
+ # before_action :set_post, only: [:show]
 
   def index
     @posts = @user.posts.order(created_at: :asc).includes(:comments, :likes).page(params[:page]).per(2)
@@ -7,5 +8,6 @@ class PostsController < ApplicationController
 
   def show
     @post = @user.posts.includes(:comments, :likes).where(id: params[:id]).first
+    raise ActiveRecord::RecordNotFound if @post.nil?
   end
 end
